8ac4a4eefed37e0f48499fd113e88eac2b17df2b
gwendolynBibah <98972629+gwendolynBibah@users.noreply.github.com>
2023-03-12 22:18:50 +0000

    Initial commit

---
+++ src/main/java/com/gwen/minibolt/MiniboltApplication.java
@@ -0,0 +1,13 @@
+package com.gwen.minibolt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniboltApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniboltApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gwen/minibolt/MiniboltApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gwen.minibolt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniboltApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
